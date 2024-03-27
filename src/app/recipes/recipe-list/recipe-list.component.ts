7c5db6cec273fff92355d12477d3c0ae456f4252
Nicholas McEnroe <nsmcenro@gmail.com>
2024-03-27 17:33:15 -0600

    Edit: Alter recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
-  recipes = [];
-
+  recipes: Recipe[] = [
+    new Recipe("Cake", "Not Real", "https://bakesbybrownsugar.com/wp-content/uploads/2021/04/Coconut-Cake-Ingredients-1-6.jpg")
+  ];
 }
